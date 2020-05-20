4267ff8c3aa47a6d7cfcdc28a2990184a4dc731d
Neicureuil <teste13300@gmail.com>
2020-05-20 16:29:52 +0200

    Add files via upload

--- recipeloader.h
+++ recipeloader.h
@@ -33,31 +33,31 @@ class RecipeLoader
 
         /**
          * Fonction qui  lit le fichier de recette en JSON
-         * @param un QFile qui est le fichier contenant la recette.
+         * @param f un QFile qui est le fichier contenant la recette.
          */
-        void LoadFromFile(QFile);
+        void LoadFromFile(QFile f);
         /**
          * Fonction qui charge une recette depuis les données JSON du QJsonObject
          */
         void LoadRecipe();
         /**
          * Fonction recupere la valeur d'une clé JSON
-         * @param un QString qui correspond a la clé recherchée.
+         * @param s un QString qui correspond a la clé recherchée.
          * @return un QString qui renvoie la valeur si la clé existe et sinon un string vide.
          */
-        QString GetString(QString);
+        QString GetString(QString s);
         /**
          * Fonction recupere la valeur d'une clé JSON
-         * @param un QString qui correspond a la clé recherchée.
+         * @param s un QString qui correspond a la clé recherchée.
          * @return un int qui renvoie la valeur si la clé existe et sinon 0.
          */
-        int GetInt(QString);
+        int GetInt(QString s);
         /**
          * Fonction convertit le format du temps PTHM en un format plus lisable.
-         * @param un QString qui correspond au temps au format PTHM.
+         * @param s un QString qui correspond au temps au format PTHM.
          * @return un QString qui correspond a temps convertie dans un format plus lisbale (heures/minutes=.
          */
-        QString GetTime(QString);
+        QString GetTime(QString s);
         /**
          * Un getter qui retourne la recette chargée.
          * @return Un Recipe* contenant recette
